c11294a04b0b316e266c0b63f75aee1fe7e014ee
torresposso <erictorres.co@gmail.com>
2024-01-05 17:03:40 -0500

    first commit

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -4,29 +4,23 @@
 
 import * as $_404 from "./routes/_404.tsx";
 import * as $_app from "./routes/_app.tsx";
-import * as $api_joke from "./routes/api/joke.ts";
+import * as $auth_callback from "./routes/auth/callback.tsx";
 import * as $auth_login from "./routes/auth/login.tsx";
 import * as $auth_oauth from "./routes/auth/oauth.tsx";
-import * as $auth_success from "./routes/auth/success.tsx";
-import * as $greet_name_ from "./routes/greet/[name].tsx";
 import * as $index from "./routes/index.tsx";
-import * as $Counter from "./islands/Counter.tsx";
+
 import { type Manifest } from "$fresh/server.ts";
 
 const manifest = {
   routes: {
     "./routes/_404.tsx": $_404,
     "./routes/_app.tsx": $_app,
-    "./routes/api/joke.ts": $api_joke,
+    "./routes/auth/callback.tsx": $auth_callback,
     "./routes/auth/login.tsx": $auth_login,
     "./routes/auth/oauth.tsx": $auth_oauth,
-    "./routes/auth/success.tsx": $auth_success,
-    "./routes/greet/[name].tsx": $greet_name_,
     "./routes/index.tsx": $index,
   },
-  islands: {
-    "./islands/Counter.tsx": $Counter,
-  },
+  islands: {},
   baseUrl: import.meta.url,
 } satisfies Manifest;
 
